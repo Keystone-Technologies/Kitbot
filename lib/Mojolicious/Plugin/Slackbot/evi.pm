1de826f22029a0830ef168f8a9b9bb5833c1efd2
Stefan Adams <sadams@keystone-it.com>
2014-11-24 15:05:14 +0000

    fixed bugs

--- lib/Mojolicious/Plugin/Slackbot/evi.pm
+++ lib/Mojolicious/Plugin/Slackbot/evi.pm
@@ -5,12 +5,23 @@ use Mojo::Base 'Mojolicious::Plugin::Slackbot::Base';
 #mojo get https://evi.com/q/what_is_the_age_of_obama 'div.single_answer div.tk_object h3.tk_text' text
 sub render {
   my $self = shift;
-  return unless $ip;
-  $self->self->ua->get("http://ipinfo.io/$ip/json" => sub {
-    my ($ua, $tx) = @_;
-    my $org = $tx->res->json->{org} || 'Unknown';
-    $self->self->render(json => {text => "$ip belongs to $org"});
-  });
+  return unless my $q = $self->text;
+  $q =~ s/\W*evi\W*//g;
+  $q =~ s/\s+/_/g;
+  my $tx = $self->c->ua->get("https://evi.com/q/$q");
+  my $answer;
+  if ( $answer = $tx->res->dom->at('div.single_answer div.tk_object h3.tk_text') ) {
+    $answer = $answer->text;
+  } elsif ( $answer = $tx->res->dom->at('div.tk_common a') ) {
+    $answer = $answer->text;
+  } else {
+    if ( 1 ) {
+      return;
+    else {
+      $answer = 'Unknown';
+    }
+  }
+  $self->respond(sprintf '%s @%s', $answer, $self->user_name);
 }
 
 1;
